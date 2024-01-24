605cda49cfdc6ebebe20fd7bc05f458eec04780a
aissa zerrad <jesus.jesus.core@gmail.com>
2024-01-24 10:06:50 -0800

    fixing let me in !

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
 file { '/etc/ssh/sshd_config':
-	ensure => present,
+  ensure  => present,
+  content => epp('puppet/templates/sshd_config.epp'),
 }
 
-file_line { 'Turn off passwd auth':
-	path   => '/etc/ssh/sshd_config',
-	line   => 'PasswordAuthentication no',
-	match  => '^#?PasswordAuthentication.*',
-}
-
-file_line { 'Declare identity file':
-	path   => '/etc/ssh/ssh_config',
-	line   => 'IdentityFile ~/.ssh/school',
-	match  => '^#?IdentityFile.*',
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => epp('puppet/templates/ssh_config.epp'),
 }
