31c74483b3de15a74dc0df6d156406e685222e6f
aissap <jesus.jesus.core@gmail.com>
2024-02-20 02:48:51 +0100

    .\2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Retrieves user information and their TODO list
+from the JSONPlaceholder API ."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base_url + "users/{}".format(user_id)).json()
+    user_name = user.get("username")
+    todos = requests.get(base_url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": user_name
+            } for t in todos]}, jsonfile)
