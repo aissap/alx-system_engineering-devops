e8df28ecf3f4c816e79170139a59f12b0eb291cb
aissap <jesus.jesus.core@gmail.com>
2024-02-20 02:20:27 +0100

    fixing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,25 +2,15 @@
 """
 Returns to-do list information for a given employee ID.
 """
-
 import requests
 import sys
 
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
+    todo_data = requests.get(base_url + "todos", params={"userId": sys.argv[1]}).json()
 
-    employee_id = sys.argv[1]
-    user_response = requests.get(base_url + "users/{}".format(employee_id))
-
-    todo_response = requests.get(base_url + "todos", params={"userId": employee_id})
-
-    user_data = user_response.json()
-    todo_data = todo_response.json()
-
-    completed_tasks = [task.get("title") for task in todo_data if task.get("completed")]
-
+    completed_tasks = [t.get("title") for t in todo_data if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user_data.get("name"), len(completed_tasks), len(todo_data)))
-
-    for task_title in completed_tasks:
-        print("\t{}".format(task_title))
+        user.get("name"), len(completed_tasks), len(todo_data)))
+    [print("\t {}".format(c)) for c in completed_tasks]
