37b53b293b30383b07c574ef40143c06d7638c73
aissap <jesus.jesus.core@gmail.com>
2024-03-05 14:48:08 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-0-subs module
-"""
+"""Queries the Reddit API and returns the number
+of subscribers for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number
-    of subscribers for a given subreddit.
-    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
