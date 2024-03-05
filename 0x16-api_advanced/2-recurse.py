498800bc28e6f2c5d58b72b0e6bda74cb726af96
aissap <jesus.jesus.core@gmail.com>
2024-03-05 15:50:08 -0800

    editing

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,31 +6,29 @@ the titles of all hot articles for a given subreddit.
 """
 
 
-def recurse(subreddit, list=[], after=None):
+def recurse(subreddit, hot_list=[]):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json().get("data", {})
-        children = data.get("children", [])
-        if children:
-            for child in children:
-               list.append(child["data"]["title"])
-            after = data.get("after")
-            if after:
-                return recurse(subreddit, list, after)
-            return list
-        else:
-            return list
-    else:
+    params = {"limit": 100}
+    
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code != 200:
         return None
-
+    
+    data = response.json().get("data")
+    children = data.get("children")
+    
+    for post in children:
+        title = post.get("data").get("title")
+        hot_list.append(title)
+    
+    if data.get("after"):
+        params["after"] = data.get("after")
+        recurse(subreddit, hot_list)
+    
+    return len(hot_list)
 
 if __name__ == "__main__":
     subreddit = input("Enter subreddit name: ")
-    r = recurse(subreddit)
-    if r is not None:
-        print(len(r))
-    else:
-        print("None")
+    print(recurse(subreddit))
